78415bffa270e4afb3db1200013c610f9745836d
Avinash Reddy <avinashreddy.idio@gmail.com>
2024-11-28 19:49:32 -0500

    next build added

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar>
-        {children}
         </Navbar>
+        {children}
       </body>
     </html>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-
-//   output : "export",
-//   basePath : "/",
-//   images: {
-//     unoptimized: true,
-//   },
-//   /* config options here */
-// };
-
 const nextConfig: NextConfig = {
 
+  output : "export",
+  basePath : "/saferr-ai.github.io",
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
+
+// const nextConfig: NextConfig = {
+
+//   /* config options here */
+// };
 
 export default nextConfig;
