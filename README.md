3a84c9a7af5359093ea85b1617aacc07c6007921
Avinash Reddy <avinashreddy.idio@gmail.com>
2024-11-28 19:34:39 -0500

    Comming Soon Message

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# saferr-ai
-AI research lab at UCF, FL. Working on Alignment of Generative AI --- Langauge Models, Diffusion Models
+# SafeRR AI Lab Website
+
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-background flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl text-foreground">
+        SafeRR AI Lab
+      </h1>
+      <p>
+        Coming Soon 
+      </p>
+    </div>
+
+  );
+}
