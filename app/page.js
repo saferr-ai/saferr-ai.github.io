8fa75e55a26c74960c207d5f09999203704c7151
Avinash Reddy <avinashreddy.idio@gmail.com>
2024-11-28 23:46:26 -0500

    navbar perfected

--- app/page.js
+++ app/page.js
@@ -2,13 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-background flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl text-foreground">
-        SafeRR AI Lab
-      </h1>
-      <p>
-        Coming Soon 
-      </p>
+    <div className=" bg-[#A0E4B0] ">
+
+      <div className="max-w-6xl mx-auto h-screen text-foreground justify-center items-center flex md:flex-row flex-col">
+      <div className="w-1/2 p-4">
+        <h1>
+          SafeRR AI lab
+        </h1>
+      </div>
+
+      <div class="flex flex-col items-start space-y-4 p-4 w-1/2">
+        <h1 class="text-xl font-bold">Pledged to align Generative AI towards</h1>
+        <p class="text-4xl font-bold">Safe</p>
+        <p class="text-4xl font-bold">Reliable</p>
+        <p class="text-4xl font-bold">Robust</p>
+        <p class="text-lg mt-4">Usage</p>
+      </div>
+      </div>
+
+      
     </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const links = [
     { href: "/research/", label: "Research" },
@@ -22,67 +23,81 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-background text-foreground">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between items-center py-4">
-                    {/* Logo */}
-                    <div className="text-2xl font-bold">
-                        <Link href="/" className="hover:text-foreground">
-                            SafeRR AI Lab
-                        </Link>
-                    </div>
-
-                    {/* Links (Desktop) */}
-                    <div className="hidden md:flex space-x-8 text-lg font-medium">
-                        {links.map((link) => (
-                            <Link
-                                key={link.href}
-                                href={link.href}
-                                className="hover:text-foreground relative group"
-                            >
-                                <span className="underline-animation">{link.label}</span>
+        <nav className="bg-background text-foreground sticky top-0 z-50">
+            <div className="absolute w-full bg-background">
+                <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex justify-between items-center py-4">
+                        {/* Logo */}
+                        <div className="text-2xl font-bold">
+                            <Link href="/" className="hover:text-foreground">
+                                SafeRR AI Lab
                             </Link>
-                        ))}
-                    </div>
+                        </div>
 
-                    {/* Mobile Menu Button */}
-                    <div className="md:hidden">
-                        <button
-                            type="button"
-                            onClick={handleMenuToggle}
-                            className="text-foreground hover:text-foreground focus:outline-none"
-                            aria-label="Toggle Menu"
-                        >
-                            <svg
-                                className="h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
+                        {/* Links (Desktop) */}
+                        <div className="hidden md:flex space-x-8 text-lg font-medium">
+                            {links.map((link) => (
+                                <Link
+                                    key={link.href}
+                                    href={link.href}
+                                    className="hover:text-foreground relative group"
+                                >
+                                    <span className="underline-animation">{link.label}</span>
+                                </Link>
+                            ))}
+                        </div>
+
+                        {/* Mobile Menu Button */}
+                        <div className="md:hidden">
+                            <button
+                                type="button"
+                                onClick={handleMenuToggle}
+                                className="text-foreground hover:text-foreground focus:outline-none"
+                                aria-label="Toggle Menu"
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M4 6h16M4 12h16m-7 6h7"
-                                />
-                            </svg>
-                        </button>
+                                <svg
+                                    className="h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M4 6h16M4 12h16m-7 6h7"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
+            </div>
 
-                {/* Mobile Menu */}
+            {/* Mobile Menu */}
+            <AnimatePresence>
                 {isMobileMenuOpen && (
-                    <div className="md:hidden">
-                        <div className="space-y-4 mt-4">
+                    <motion.div
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: "auto", opacity: 1 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        className="absolute  top-16 left-0 w-full bg-background shadow-lg"
+                    >
+                        <div className="space-y-4 px-4 pb-4 mx-auto">
                             {links.map((link, index) => (
-                                <div
+                                <motion.div
                                     key={link.href}
-                                    className="transition-transform duration-300 ease-in-out transform"
-                                    style={{
-                                        transitionDelay: `${index * 50}ms`,
+                                    initial={{ opacity: 0, x: -20 }}
+                                    animate={{ opacity: 1, x: 0 }}
+                                    exit={{ opacity: 0, x: -20 }}
+                                    transition={{
+                                        delay: index * 0.05,
+                                        duration: 0.2,
                                     }}
+                                    className="transform transition-transform duration-300 ease-in-out"
                                 >
                                     <Link
                                         href={link.href}
@@ -91,12 +106,12 @@ const Navbar = () => {
                                     >
                                         {link.label}
                                     </Link>
-                                </div>
+                                </motion.div>
                             ))}
                         </div>
-                    </div>
+                    </motion.div>
                 )}
-            </div>
+            </AnimatePresence>
         </nav>
     );
 };
