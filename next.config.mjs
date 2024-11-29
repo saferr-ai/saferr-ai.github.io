a7ff1486e7bbdf07a0ee3a243a26876fb9384ee8
Avinash Reddy <avinashreddy.idio@gmail.com>
2024-11-28 20:39:24 -0500

    base path added

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: "export",
+const nextConfig = {
+    output: "export",
 
  
-//   basePath: "/saferr-ai.github.io",
+  basePath: "/saferr-ai.github.io",
 
-//   /**
-//    * Disable server-based image optimization. Next.js does not support
-//    * dynamic features with static exports.
-//    *
-//    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-//    */
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-const nextConfig = {
-
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
+
+// const nextConfig = {
+
+// };
 
 export default nextConfig;
