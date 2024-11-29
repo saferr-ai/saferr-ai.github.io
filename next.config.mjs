c6e80d842482490955f2ae4d89273a4195f3ca37
Avinash Reddy <avinashreddy.idio@gmail.com>
2024-11-28 20:39:10 -0500

    updated export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {
+//     output: "export",
+
+ 
+//   basePath: "/saferr-ai.github.io",
+
+//   /**
+//    * Disable server-based image optimization. Next.js does not support
+//    * dynamic features with static exports.
+//    *
+//    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+//    */
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+const nextConfig = {
+
+};
 
 export default nextConfig;
