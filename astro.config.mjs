9fbc23f75016dba60063725877aaacbc0b2c994b
saferr-ai <saferraiucf@gmail.com>
2026-03-12 16:27:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import react from '@astrojs/react';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://saferrai-lab.org',  // ← add this
   integrations: [
     tailwind(),
     mdx(),
